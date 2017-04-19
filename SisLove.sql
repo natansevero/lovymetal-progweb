085f8c68ff5037e58abdd53b3943f4242029e3a3
natansevero <natanseverolp@gmail.com>
2017-04-19 12:04:52 -0300

    Modificando sql

--- SisLove.sql
+++ SisLove.sql
@@ -1,63 +1,81 @@
-﻿CREATE TABLE Usuario(
-	login VARCHAR(30) UNIQUE,
-	senha VARCHAR(13) UNIQUE NOT NULL,
-	nome VARCHAR(100) NOT NULL,
-	apelido VARCHAR (80),
-	datanasc DATE NOT NULL,
-	cidade VARCHAR(80) NOT NULL,
-	email VARCHAR(80) NOT NULL,
-	profissao VARCHAR(80) NOT NULL,
-	descricao VARCHAR (200),
-	sexo VARCHAR(2) NOT NULL,
-	status VARCHAR(3) UNIQUE NOT NULL,
-	altura REAL NOT NULL,
-	peso REAL NOT NULL,
-	cabelo VARCHAR(20) NOT NULL,
-	fotoperfil VARCHAR(500),
-	PRIMARY KEY(login)
+﻿CREATE TABLE usuario(
+        id serial,
+	nome_completo varchar(50) not null,
+	apelido varchar(10) not null,
+	data_nasc varchar(10) not null,
+	cidade varchar(15) not null,
+	estado varchar(2) not null,
+	email varchar(50) not null unique,
+	senha text not null,
+	profissao varchar(20) not null,
+	descricao text not null,
+	status varchar(10) not null,
+	altura double precision not null,
+	cor_cabelo varchar(10) null,
+	sexo varchar(1) not null,
+	foto_perfil varchar(50) not null,
+	PRIMARY KEY(id)
 );
 
-CREATE TABLE Amizade(
-	usuario VARCHAR(30),
-	amigo VARCHAR(30),
-	PRIMARY KEY(usuario, amigo),
-	FOREIGN KEY (usuario) REFERENCES Usuario(login),
-	FOREIGN KEY (amigo) REFERENCES Usuario(login) ON DELETE CASCADE
+CREATE TABLE passatempo (
+	id_usuario int,
+	passatempo VARCHAR(80),
+	PRIMARY KEY(id_usuario),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE Galeria(
-	id VARCHAR(30),
-	usuariologin VARCHAR(30),
+CREATE TABLE galeria(
+	id_galeria serial,
+	id_usuario int,
 	nomegaleria VARCHAR(100) NOT NULL,
 	foto VARCHAR(500),
-	PRIMARY KEY (id),
-	FOREIGN KEY (usuariologin) REFERENCES Usuario(login) ON DELETE CASCADE
+	PRIMARY KEY (id, id_usuario),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Mensagem(
-	mensagemid VARCHAR(30),
-	remetente VARCHAR(30),
-	destinatario VARCHAR(30),
-	mensagem VARCHAR(1000),
-	PRIMARY KEY (mensagemid),
-	FOREIGN KEY (remetente) REFERENCES Usuario(login) ON DELETE CASCADE,
-	FOREIGN KEY (destinatario) REFERENCES Usuario(login) ON DELETE CASCADE
+CREATE TABLE amizade(
+	id_usuario int,
+	id_amigo int,
+	PRIMARY KEY(id_usuario, id_amigo),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+	FOREIGN KEY (id_amigo) REFERENCES usuario(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Passatempos(
-	usuariologin VARCHAR(30),
-	passatempo VARCHAR(80),
-	PRIMARY KEY(usuariologin, passatempo),
-	FOREIGN KEY (usuariologin) REFERENCES Usuario(login) ON DELETE CASCADE
+CREATE TABLE solicita_amizade(
+        id_solicitador int,
+        id_solicitante int,
+        status int not null,
+        primary key(id_solicitador, id_solicitante),
+        FOREIGN KEY (id_solicitador) REFERENCES usuario(id),
+	FOREIGN KEY (id_solicitante) REFERENCES usuario(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Relacionamento(
-	usuariologin VARCHAR(30),
+CREATE TABLE mensagem(
+	id serial,
+	id_remetente int,
+	id_destinatario int,
+	mensagem VARCHAR(1000) not null,
+        status int not null,
+	PRIMARY KEY (id, id_remetente, id_destinatario),
+	FOREIGN KEY (id_remetente) REFERENCES usuario(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_destinatario) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+CREATE TABLE relacionamento(
+	id_usuario int,
 	tipo VARCHAR(10) NOT NULL,
-	par VARCHAR(30),
-	PRIMARY KEY(usuariologin, par),
-	FOREIGN KEY (usuariologin) REFERENCES Usuario(login),
-	FOREIGN KEY (par) REFERENCES Usuario(login) ON DELETE CASCADE
-)
+	par int,
+	PRIMARY KEY(id_usuario, par),
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+	FOREIGN KEY (par) REFERENCES usuario(id) ON DELETE CASCADE
+);
+
+CREATE TABLE postagem(
+        id serial,
+        id_usuario int,
+        descricao text,
+        foto text not null,
+        primary key(id, id_usuario),
+        foreign key(id_usuario) references usuario(id) on delete cascade
+);
 	
