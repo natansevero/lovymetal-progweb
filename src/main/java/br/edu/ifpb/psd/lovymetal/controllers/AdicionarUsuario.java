7bdc0f4f07720c157351a006fe0d1d84bffe398d
natansevero <natanseverolp@gmail.com>
2017-04-21 18:35:30 -0300

    Modificando SQL e finalizando cadastro de usuário

--- src/main/java/br/edu/ifpb/psd/lovymetal/controllers/AdicionarUsuario.java
+++ src/main/java/br/edu/ifpb/psd/lovymetal/controllers/AdicionarUsuario.java
@@ -8,11 +8,17 @@ package br.edu.ifpb.psd.lovymetal.controllers;
 
 import br.edu.ifpb.psd.lovymetal.facade.FacadeFactory;
 import br.edu.ifpb.psd.lovymetal.facade.FacadeIF;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.util.Base64;
+import java.util.List;
 
 /**
  *
@@ -28,11 +34,44 @@ public class AdicionarUsuario implements CommandIF {
     
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        // request getParameter's
-        // Temporario <=> Simbolico
-        facade.adicionarUsuario(0, null, null, null, null, null, null, null, null, null, 0, 0, null, null);
-        RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("login.jsp");
-        dispatcher.forward(req, res);
+        String nome_completo = req.getParameter("nome_completo");
+        String apelido = req.getParameter("apelido");
+        String email = req.getParameter("email");
+        String senha = req.getParameter("senha");
+        String sexo = req.getParameter("sexo");
+        String data_nasc = req.getParameter("data_nasc");
+        String cidade = req.getParameter("cidade");
+        String profissao = req.getParameter("profissao");
+        String descricao = req.getParameter("descricao");
+        String status = req.getParameter("status");
+        double altura = Double.parseDouble(req.getParameter("altura"));
+        String cor_cabelo = req.getParameter("cor_cabelo");
+        Part foto_perfil = req.getPart("foto_perfil");
+        
+        
+        String appPath = req.getServletContext().getRealPath("");
+        String upPath = appPath + "/imagens" + File.separator + apelido;
+        System.out.println(upPath);
+        File up = new File(upPath);
+        if(!up.exists()) up.mkdirs();
+        
+        System.out.println(foto_perfil.getSubmittedFileName());
+        
+        String caminho = "imagens/" + apelido + "/" + foto_perfil.getSubmittedFileName(); 
+        
+        System.out.println(caminho);
+        
+        List<Part> list = (List) req.getParts();
+        
+        for(Part p : list) {
+            System.out.println(req.getParameter(p.getName()));
+        }
+                
+        facade.adicionarUsuario(senha, nome_completo, apelido, data_nasc, cidade, email, profissao, descricao, sexo, status, altura, cor_cabelo, caminho);
+            
+//        RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("home.jsp");
+//        dispatcher.forward(req, res);
+        res.sendRedirect("index.html");
     }
     
 }
