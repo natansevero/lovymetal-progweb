0d61b00bbee16ba92f6c95ec5153dc92606c3810
natansevero <natanseverolp@gmail.com>
2017-05-11 16:39:03 -0300

    Adicionando email do usuário logado na sessão

--- src/main/java/br/edu/ifpb/psd/lovymetal/filters/FilterID.java
+++ src/main/java/br/edu/ifpb/psd/lovymetal/filters/FilterID.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifpb.psd.lovymetal.Filters;
+package br.edu.ifpb.psd.lovymetal.filters;
 
 import java.io.IOException;
 import javax.servlet.Filter;
