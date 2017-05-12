53ca694a7b5d30878fb7ecb138d74c5d6a3f63d4
natansevero <natanseverolp@gmail.com>
2017-05-12 14:35:08 -0300

    Resolvendo conflitos

--- src/main/java/br/edu/ifpb/psd/lovymetal/facade/FacadePost.java
+++ src/main/java/br/edu/ifpb/psd/lovymetal/facade/FacadePost.java
@@ -6,6 +6,7 @@
 package br.edu.ifpb.psd.lovymetal.facade;
 
 import br.edu.ifpb.psd.lovymetal.DAO.managers.GerenciadorPostagem;
+import br.edu.ifpb.psd.lovymetal.entidades.Mensagem;
 import br.edu.ifpb.psd.lovymetal.entidades.Postagem;
 import br.edu.ifpb.psd.lovymetal.entidades.Usuario;
 import java.util.List;
@@ -58,5 +59,50 @@ public class FacadePost implements FacadeIF{
     @Override
     public void excluiPostagem(int postagem_id, int id_usuario) {
         gerenciadorpostagem.excluiPostagem(postagem_id, id_usuario);
+    }
+
+    @Override
+    public void novoPassatempo(int id_usuario, String passatempo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removePassatempo(int id_usuario, String passatempo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void novaGaleria(int galeria_id, int id_usuario, String nomegaleria, String foto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeGaleria(int galeria_id, int id_usuario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void enviarMensagem(int mensagemid, int remetente, int destinatario, String mensagem, int status) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Mensagem> verificarMensagens(int destinatario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluirMensagem(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void novaRelacao(int id_usuario, String tipo, int par) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeRelacao(int id_usuario, int par) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
